616eea6e9b103af2b8e70591da9f41f865216332
Turtled <trtld2@gmail.com>
2019-11-19 16:07:51 -0800

    added img_url column to rentals, updated docs

--- data/seeds/rentals.js
+++ data/seeds/rentals.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('rentals').insert([
-        {id: 1, user_id: 1, item_name: "Expensive Camara", item_description: "An expensive camara that produces high quality photos.", category: "Electronical", rate: 20.50, rented_at: "12-12-1526-23:20", due_back: "13-12-1526-23:20", renter_id: 2},
-        {id: 2, user_id: 2, item_name: "Average Quality Laptop", item_description: "An average laptop.", category: "Computronical", rate: 200.50, renter_id: -1},///renter_id -1 not rented
-        {id: 3, user_id: 3, item_name: "Smartphone", item_description: "Hey, wanna rent my smartphone?", renter_id: -1}
+        {id: 1, user_id: 1, item_name: "Expensive Camara", item_description: "An expensive camara that produces high quality photos.", category: "Electronical", rate: 20.50, rented_at: "12-12-1526-23:20", due_back: "13-12-1526-23:20", renter_id: 2, img_url: "https://as.com/betech/imagenes/2018/02/27/portada/1519755076_817147_1519762876_noticia_normal.jpg"},
+        {id: 2, user_id: 2, item_name: "Average Quality Laptop", item_description: "An average laptop.", category: "Computronical", rate: 200.50, renter_id: null, img_url: "https://i.ytimg.com/vi/gtpt6vDpfl0/hqdefault.jpg"},///renter_id -1 not rented
+        {id: 3, user_id: 3, item_name: "Smartphone", item_description: "Hey, wanna rent my smartphone?", renter_id: null, img_url: "https://spectator.imgix.net/content/uploads/2018/12/MW.jpg?auto=compress,enhance,format&crop=faces,entropy,edges&fit=crop&w=820&h=550"}
       ]);
     });
 };
